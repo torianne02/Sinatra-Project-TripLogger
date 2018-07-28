dfc2e004cdd33e776591ee6ef90b13ebbab55958
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-28 11:19:18 -0700

    Adds forms to login and signup Sets up application_controller and helper methods deletes Corneal icon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,21 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
