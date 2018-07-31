c6a8559337c5ea1c25b3c1cf380a39fbd8d5bb97
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-31 12:32:00 -0700

    Add flash messages to users_controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,29 @@
 class UsersController < ApplicationController
-  get '/users/signup' do
-    if !session[:user_id]
-      erb :'/users/signup'
+  get '/signup' do
+    if logged_in?
+      redirect "/users/#{@user.id}" # unsure if I want to redirect here or /cities
     else
-      redirect '/users/:id' # unsure if I want to redirect here or /cities
+      erb :'/users/signup'
     end
   end
 
   post '/signup' do
-    if params.values.include?("")
-      flash[:message] = "Oops! Make sure to fill in all criteria."
-      redirect '/users/signup'
-    else
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    if @user.valid?
       session[:user_id] = @user.id
-      redirect '/users/:id'
+      redirect "/users/#{@user.id}"
+    elsif @user.invalid? && User.find_by(username: @user.username)
+      flash[:message] = "Sorry, that username is already taken."
+      redirect '/signup'
+    else
+      flash[:message] = "Oops! Make sure to fill in all criteria."
+      redirect '/signup'
     end
   end
 
-  get '/users/login' do
+  get '/login' do
     if logged_in?
-      redirect '/users/:id'
+      redirect "/users/#{@user.id}"
     else
       erb :'/users/login'
     end
@@ -29,9 +32,13 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by(username: params[:username])
 
-    if @user != nil
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect '/users/:id'
+      flash[:message] = "Welcome back, #{@user.username}!"
+      redirect "/users/#{@user.id}"
+    else
+      flash[:message] = "Invalid username or password. Please try again."
+      redirect '/login'
     end
   end
 
@@ -43,7 +50,7 @@ class UsersController < ApplicationController
   get '/logout' do
     if logged_in?
       session.clear
-      redirect '/users/login'
+      redirect '/login'
     else
       redirect '/welcome'
     end
