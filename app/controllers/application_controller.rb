1c4e89baf17dd51cb54125c0a767fcdcfb255b54
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-02 09:45:01 -0700

    deletes usage of rack-flash and coinciding code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 require './config/environment'
-require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
-  use Rack::Flash
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
