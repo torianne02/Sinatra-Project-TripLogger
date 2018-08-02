80cd7f7dca2b7d3205508c89d7da2e5e67d04c70
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-02 10:57:39 -0700

    updates seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-User.create(username: 'torianne', email: 'torianne26@gmail.com', password: 'password')
+User.create(username: 'torianne', email: 'torianne@gmail.com', password: 'password')
 
 User.create(username: 'kevcraw', email: 'kevcraw@gmail.com', password: 'password1')
 
 User.create(username: 'coachflu', email: 'coachflu@gmail.com', password: 'password2')
+
+User.create(username: 'torrnado', email: 'torrnado@gmail.com', password: 'tornado1')
 
 City.create(name: 'Houston')
 
@@ -23,3 +25,9 @@ Trip.create(length_of_visit: '4 days', city_id: 3, user_id: 2)
 Trip.create(length_of_visit: '2 days', city_id: 4, user_id: 3)
 
 Trip.create(length_of_visit: '2 days', city_id: 5, user_id: 2)
+
+Trip.create(length_of_visit: '1 week', city_id: 2, user_id: 4)
+
+Trip.create(length_of_visit: '2 months', city_id: 4, user_id: 4)
+
+Trip.create(length_of_visit: '6 days' city_id: 3, user_id: 3)
